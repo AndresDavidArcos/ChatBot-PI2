3f4b1dbf45fb0985e4aa3db605a5807127671de3
Sergio Escudero Tabares <sergio.escudero@correounivalle.edu.co>
2024-03-05 15:44:16 -0500

    Agregado de importación y uso de la biblioteca de IA generativa de Google en el chatbot

--- js/chatbot.js
+++ js/chatbot.js
@@ -20,62 +20,9 @@ const createChatLi = (message, className) => {
     chatLi.querySelector('p').textContent = message;
     return chatLi;
 };
-
-const generateResponse = (incomingChatLi) => {
-    const API_URL = 'https://api.openai.com/v1/chat/completions';
-    const messageElement = incomingChatLi.querySelector('p');
-
-    const requestOptions = {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${API_KEY}`,
-        },
-        body: JSON.stringify({
-            model: 'gpt-3.5-turbo',
-            message: [{ role: 'user', content: userMessage }],
-        }),
-    };
-    fetch(API_URL, requestOptions)
-        .then((res) => res.json())
-        .then((data) => {
-            console.log(data);
-            messageElement.textContent = data.choices[0].message.content;
-        })
-        .catch((error) => {
-            messageElement.classList.add('error');
-            messageElement.textContent = 'Oops! Intenta de nuevo!';
-            console.log(error);
-        })
-        .finally(() => chatBox.scrollTo(0, chatBox.scrollHeight));
-};
-
-const handleChat = () => {
-    userMessage = chatInput.value.trim();
-    if (!userMessage) return;
-    chatInput.value = '';
-    chatInput.style.height = `${inputInitHeight}px`;
-
-    chatBox.appendChild(createChatLi(userMessage, 'outgoing'));
-    chatBox.scrollTo(0, chatBox.scrollHeight);
-
-    setTimeout(() => {
-        const incomingChatLi = createChatLi('Thinking...', 'incoming');
-        chatBox.appendChild(incomingChatLi);
-        chatBox.scrollTo(0, chatBox.scrollHeight);
-        generateResponse(incomingChatLi);
-    }, 600);
-};
-
 chatInput.addEventListener('input', () => {
     chatInput.style.height = `${inputInitHeight}px`;
     chatInput.style.height = `${chatInput.scrollHeight}px`;
-});
-chatInput.addEventListener('keydown', (e) => {
-    if (e.key === 'Enter' && !e.shiftKey && window.innerWidth > 800) {
-        e.preventDefault();
-        handleChat();
-    }
 });
 chatbotToggle.addEventListener('click', () =>
     document.body.classList.toggle('show-chatbot')
@@ -83,4 +30,3 @@ chatbotToggle.addEventListener('click', () =>
 chatbotCloseBtn.addEventListener('click', () =>
     document.body.classList.remove('show-chatbot')
 );
-sendChatBtn.addEventListener('click', handleChat);
